2c7789441958b8f2edfb8a65b8db4a8d6fd9f0ba
embed <brydon@bubblez.xyz>
2020-05-09 15:20:53 -0400

    Remove MySQL support

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ const fs = require('fs');
 // Initialize urlencoder for express, Initialize Random String Generator.
 var urlencoded = bodyparser.urlencoded({extended:false});
 var randomstr = require('randomstring');
+// Initialize Configuration
+const config = require('conf.json');
+var postgres = require('postgres');
+var sql = postgres({
+    host: config.db_login.host,
+    port: config.db_login.port,
+    database: config.db_login.database,
+    username: config.db_login.username,
+    password: config.db_login.password,
+    connection : {
+        application_name: "FirefiCDS"
+    }
+});
 
 app.listen()
 
