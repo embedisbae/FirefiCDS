85595ca8a576e1d94d9293e69ea99548c60a3bd8
embed <brydon@bubblez.xyz>
2020-05-09 14:25:59 -0400

    Add FirefiCDS Base
    Add base files and configuration.

---
+++ main.js
@@ -0,0 +1,18 @@
+// Initialize Express, Express Uploader, Body Parser, File System
+const app = require('express').express();
+const upload = require('express-fileupload');
+const bodyparser = require('body-parser');
+const fs = require('fs');
+// Initialize urlencoder for express, Initialize Random String Generator.
+var urlencoded = bodyparser.urlencoded({extended:false});
+var randomstr = require('randomstring');
+
+app.listen()
+
+app.get('/g/:uid/:cid', function(req,res){
+
+});
+
+app.post('/up', function(req,res){
+
+});
